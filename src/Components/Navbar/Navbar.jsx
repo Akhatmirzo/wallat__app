8660bc16dfceb4354791e1a9091bfbcb2b4c8c80
khojiakbarr <chinozbekov09@gmail.com>
2024-03-24 00:00:14 +0500

    sdasd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,6 +25,7 @@ export default function Navbar() {
             >
               Receve
             </button>
+            
           </Link>{" "}
           <Link to="/Send">
             <button
